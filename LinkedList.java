ae02d57b660ee48a41dbca5b9e9528a66b4fb7b8
Kojjavarapu Sanjay Kumar <84496961+Sanjaykumar-95@users.noreply.github.com>
2023-11-15 14:50:10 +0530

    Add files via upload

--- LinkedList.java
+++ LinkedList.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Node {
   Node next;
   int data;
@@ -83,6 +85,7 @@ class Linked {
 
     Node temp=node;
     int i=1;
+    k=k%size;
 
     while(i<size-k){
       temp=temp.next;
@@ -101,6 +104,109 @@ class Linked {
 
     return head;
   }
+
+  //RotateLinked List Anti Clockwise
+  public Node AntiRotateClockWise(int k,Node node){
+    if(node==null || k<=0) return node;
+    
+    int size=getSize(node);
+
+    Node temp=node;
+    int i=1;
+    k=k%size;
+
+    while(i<k){
+      temp=temp.next;
+      i++;
+    }
+
+    Node ans=temp.next;
+    Node head=ans;
+    temp.next=null;
+
+    while(ans.next!=null){
+      ans=ans.next;
+    }
+
+    ans.next=node;
+
+    return head;
+  }
+
+
+  //Middle node of the LinkedList
+  public int MiddleNodeVal(Node node){
+    if(node==null) return 0;
+    Node t=node;
+    Node r=node;
+
+    while(t.next!=null && r.next!=null){
+      t=t.next;
+      r=r.next.next;
+    }
+    return t.data;
+  }
+
+  //Reverse LinkedList
+  public Node ReverseLinkedList(Node node){
+    Node prev=null;
+    Node curr=node;
+
+    while (curr!=null) {
+      Node next=curr.next;
+      curr.next=prev;
+      prev=curr;
+      curr=next;
+    }
+    return prev;
+  }
+
+
+  public Node MiddleNode(Node node){
+    if(node==null) return null;
+    Node a = node;
+    Node b = node.next;
+    
+    while(b != null && b.next != null) {
+      a = a.next;
+      b = b.next.next;
+    }
+    return a;
+  }
+
+  //Sort LinkedList used Merge Sort
+  public Node mergeSort(Node node) {
+    if(node == null || node.next == null) {
+      return node;
+    }
+    
+    Node middle = MiddleNode(node);
+    Node secondHalf = middle.next;
+    middle.next = null;
+    
+    return merge(mergeSort(node), mergeSort(secondHalf));
+  }
+
+  public static Node merge(Node a, Node b){
+    Node temp=new Node();
+    Node ans=temp;
+
+    while (a!=null && b!=null) {
+      if(a.data<b.data){
+        temp.next=a;
+        a=a.next;
+      }
+      else{
+        temp.next=b;
+        b=b.next;
+      }
+      temp=temp.next;
+    }
+    temp.next=(a==null)?b:a;
+
+    return ans.next;
+  }
+
 
 
   //Give value exist in linkedlist using reursion
@@ -183,6 +289,25 @@ public class LinkedList {
     root=a.RotateClockWise(2,root);
     System.out.println("Roate linkedlist Clock wise");
     a.printList(root);
+    System.out.println();
+
+    root=a.AntiRotateClockWise(2, root);
+    System.out.println("Roate linkedlist Anti-Clock wise");
+    a.printList(root);
+    System.out.println();
+
+    System.out.println("Middle node of LinkedList: "+a.MiddleNodeVal(root));
+    System.out.println();
+
+    root=a.ReverseLinkedList(root);
+    System.out.println("Reverse of LinkedList: ");
+    a.printList(root);
+    System.out.println();
+
+    root = a.insert(12, root);
+    Node sorted=a.mergeSort(root);
+    System.out.println("Sorting LinkedList: ");
+    a.printList(sorted);
     System.out.println();
 
   }
